889fd669babdee61c99fc35528d6b2dd93095ef5
kvong92 <vongkevin.c92@gmail.com>
2023-10-18 11:27:02 +0200

    init db + test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getUsers, dbConnect } from '../src/Firebase/firebase'
+
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
 export default function App() {
+  const db = dbConnect();
+  getUsers(db);
+
   return (
     <h1 className="text-3xl font-bold ">
       Hello world!
---
+++ src/Firebase/firebase.ts
@@ -0,0 +1,28 @@
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, Firestore } from 'firebase/firestore/lite';
+
+export function dbConnect() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDpFfg30TUwjZ9Nf-7Iih0_fmvtavJobrE",
+        authDomain: "l2-tv-series-tracker.firebaseapp.com",
+        projectId: "l2-tv-series-tracker",
+        storageBucket: "l2-tv-series-tracker.appspot.com",
+        messagingSenderId: "312498893101",
+        appId: "1:312498893101:web:f38243129069d38acc63a4"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    return db;
+}
+
+export async function getUsers(db: Firestore) {
+    const usersCol = collection(db, 'users');
+    await getDocs(usersCol)
+        .then((snapshot) => {
+            const res = snapshot.docs.map(doc => doc.data());
+            console.log(res);
+            return res;
+        })
+}
