2b66ac931f94b707a43c5266532454c8ac14fb05
mathys <mathyscesaire@gmail.com>
2023-10-19 14:12:53 +0200

    feat: add filter for series

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {MouseEvent, RefObject, useEffect} from 'react';
 import './App.css';
 import SeriesCard from "./components/SeriesCard";
 import {SeriesPoster} from "./components/SeriesPoster";
 import {useSearchParams} from "react-router-dom";
 import {PaginationButton} from "./components/PaginationButton";
+import {Pill} from "./components/Pill";
 
 export interface Series {
     id: number;
@@ -35,8 +36,8 @@ export const options = {
     }
 };
 
-const getSeries = async (location: URLSearchParams) : Promise<Series[]> => {
-    return await fetch(`https://api.themoviedb.org/3/tv/popular?language=en-US&page=${location.get("page")}`, options)
+const getSeries = async (location: URLSearchParams, genreId: number = 0) : Promise<Series[]> => {
+    return await fetch(`https://api.themoviedb.org/3/tv/popular?language=en-US&page=${location.get("page")}${genreId > 0 ? `&with_genres=${genreId}` : ""}`, options)
         .then(response => response.json())
         .then(data => data.results)
         .catch(error => console.log(error))
@@ -56,38 +57,62 @@ const getTrendingSeries = async (location: URLSearchParams) : Promise<Series[]> 
         .catch(error => console.log(error))
 }
 
-
+export const onClickGenre = (e: MouseEvent<HTMLElement>, categoriesRef: RefObject<HTMLDivElement> | null = null, genreId: number, location: URLSearchParams, setLocation: (location: URLSearchParams) => void) => {
+    e.preventDefault();
+    e.stopPropagation();
+    location.set("genre", genreId.toString());
+    location.set("page", "1");
+    setLocation(location);
+    if (categoriesRef && categoriesRef.current) categoriesRef.current.scrollIntoView({behavior: "smooth"})
+}
 
 export default function App() {
-    const [location,] = useSearchParams()
+    const [location, setLocation] = useSearchParams()
     const [allSeries, setAllSeries] = React.useState<Series[]>([])
     const [allGenres, setAllGenres] = React.useState<Genre[]>([])
     const [trendingSeries, setTrendingSeries] = React.useState<Series>()
+    const categoriesRef = React.useRef<HTMLDivElement>(null)
     useEffect(() => {
         if (!location.has("page")) {
             location.set("page", "1");
         }
+        if (location.has("genre") && Number(location.get("genre")) === 0) {
+            location.delete("genre");
+            setLocation(location);
+        }
         getGenres().then(genres => setAllGenres(genres))
-        getSeries(location).then(series => setAllSeries(series))
+        if (location.has("genre")) {
+            getSeries(location, Number(location.get("genre"))).then(series => setAllSeries(series))
+        } else {
+            getSeries(location).then(series => setAllSeries(series))
+        }
         getTrendingSeries(location).then(series => setTrendingSeries(series[(Number(location.get("page")) - 1)%20]))
     }, [location])
     return (
-        <div className="flex flex-col gap-10 p-5">
+        <div className="flex flex-col p-5">
             {
                 trendingSeries && (
                     <SeriesPoster series={trendingSeries} genres={allGenres} />
                 )
             }
+            <div className="flex w-full gap-5 whitespace-nowrap flex-wrap py-10" id="genres" ref={categoriesRef}>
+                <Pill text="All" className={!location.has("genre") ? "bg-amber-200" : ""} onClick={(e) => onClickGenre(e, categoriesRef, 0, location, setLocation)} />
+                {
+                    allGenres.map((genre) => (
+                        <Pill text={genre.name} className={genre.id === Number(location.get("genre")) ? "bg-amber-200" : ""} onClick={(e) => onClickGenre(e, categoriesRef,genre.id, location, setLocation)} />
+                    ))
+                }
+            </div>
             <div className="grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5  gap-8 justify-center">
                 {allSeries.map((series) => (
-                    <SeriesCard series={series} genres={allGenres} key={`SeriesCard-${series.id}`} />
+                    <SeriesCard categoriesRef={categoriesRef} series={series} genres={allGenres} key={`SeriesCard-${series.id}`} />
                 ))}
             </div>
-            <div className="flex justify-center w-full gap-5">
+            <div className="flex justify-center w-full gap-5 py-10">
                 {Number(location.get("page")) > 1 &&
-                    <PaginationButton text="< previous page" page={-1} />
+                    <PaginationButton categoriesRef={categoriesRef} text="< previous page" page={-1} />
                 }
-                <PaginationButton text="next page >" page={1} />
+                <PaginationButton categoriesRef={categoriesRef} text="next page >" page={1} />
             </div>
         </div>
     )
--- src/components/PaginationButton.tsx
+++ src/components/PaginationButton.tsx
@@ -4,19 +4,18 @@ import { useSearchParams } from "react-router-dom";
 interface PaginationButtonProps {
     text?: string;
     page?: number;
+    categoriesRef?: React.RefObject<HTMLDivElement> | null;
 }
 
-export function PaginationButton({text = "next page >", page = 1}:PaginationButtonProps) {
+export function PaginationButton({text = "next page >", page = 1, categoriesRef = null} : PaginationButtonProps) {
     const [location, setLocation] = useSearchParams()
     return (
         <button className="bg-gray-100 rounded-md p-2" onClick={() => {
             location.set("page", ((location.has("page") ? Number(location.get("page")) : 1) + page).toString());
-            window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth"
-            });
             setLocation(location);
+            if (categoriesRef && categoriesRef.current) categoriesRef.current.scrollIntoView(
+                {behavior: "smooth"}
+            );
         }}>{text}</button>
     );
 }
