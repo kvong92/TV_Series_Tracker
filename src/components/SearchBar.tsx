b95f519f6f4b613232cd5ba1cc9a7784e108f7fc
Léandre <leandre.colard@gmail.com>
2023-10-19 20:16:31 +0200

    working & stylized searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,52 +1,39 @@
 import React, { useRef, useState } from 'react';
-import instance from '../services/API';
+import { useSearchParams } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
 
 export const SearchBar: React.FC = () => {
-  const [items, setItems] = useState<string[]>([]);
-  const [query, setQuery] = useState<string>('');
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [location, setLocation] = useSearchParams();
+  const [query, setQuery] = useState<string>(location.get('search') || '');
 
-  const filteredItems = items.filter(item =>
-    item.toLowerCase().includes(query.toLowerCase())
-  );
+  async function onSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-  async function onSearch() {
-    try {
-      const response = await instance.get(`/search/movie?language=en-US&include_adult=false&query=${query}`, {
-        params: {
-          query: query,
-        },
-      });
+    if (query.length > 0) {
+     location.set('search', query);
+     location.delete('genre');
+     location.delete('page');
 
-      const movies = response.data.results;
-      const movieTitles = movies.map((movie: any) => movie.title);
-      setItems(movieTitles);
-    } catch (error) {
-      console.error('Error searching for movies:', error);
+      setLocation(location);
+      return;
     }
+    location.delete('search');
+    setLocation(location);
   }
 
   return (
-    <div className="max-w-xl mx-auto p-4">
-      <div className="mb-4">
+    <form className="py-9 w-full h-full" onSubmit={onSearch}>
+        <button type='submit' className="p-4 focus:outline-none text-black absolute">
+          <BiSearch />
+        </button>
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           type="search"
-          placeholder="Search for a movie"
-          className="w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:border-blue-400"
+          placeholder="Type to search..."
+          className="pl-10 w-full h-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:border-blue-400"
         />
-        <button onClick={onSearch} className="mt-2 bg-blue-500 text-white px-4 py-2 rounded focus:outline-none">
-          Search
-        </button>
-      </div>
-      <h3 className="text-lg font-semibold mb-2">Items</h3>
-      <div className="flex flex-wrap">
-        {filteredItems.map(item => (
-          <div key={item} className="px-3 py-2 m-1 bg-gray-200 rounded">{item}</div>
-        ))}
-      </div>
-    </div>
+    </form>
   );
 };
 
