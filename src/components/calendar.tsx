c582559bbfbe19d94257d39b44c33993cc1e4bf0
Lucastor05 <lucasdeniel05@gmail.com>
2023-10-19 23:00:59 +0200

    calendrier

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { getSeriesDetails } from "./SeriesPoster";
 
-export default function Calendrier() {
+export default function Calendrier({ list_fav_series }:any) {
 
-    const [favSeries, setFavSeries] = useState<any[]>([]);
+    const [list_series, setListSeries] = useState<any>([]);
 
     useEffect(() => {
         
-    }, []);
-
+    }, []); 
 
     return (
         <div className="flex flex-col items-start gap-6 self-stretch">
