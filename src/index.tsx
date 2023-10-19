b18d391fd3c4f1ffeef2391f372071d245aba46d
Lucastor05 <lucasdeniel05@gmail.com>
2023-10-19 09:48:54 +0200

    merge develop

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/develop
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -19,7 +15,3 @@ root.render(
       </BrowserRouter>
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/develop
