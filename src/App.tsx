0f6a400595b2dc4e8c91f4092ba62e75486d6dda
Lucastor05 <lucasdeniel05@gmail.com>
2023-10-19 09:45:22 +0200

    debut detail serie

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-// import React from 'react';
-// import './App.css';
-
-// export default function App() {
-//   return (
-//     <h1 className="text-3xl font-bold ">
-//       Hello world!
-//     </h1>
-//   )
-// }
-
-
 import React from 'react';
 import './App.css';
-import DetailSerie from './components/detail_serie';
-import Connexion from './components/connexion';
 
 export default function App() {
   return (
-    <div>
-      <Connexion />
-    </div>
+    <h1 className="text-3xl font-bold ">
+      Hello world!
+    </h1>
   )
 }
