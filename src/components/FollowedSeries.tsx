074e2473c23f2ef4e55013857dedd44ead43c742
Léandre <leandre.colard@gmail.com>
2023-10-20 14:38:40 +0200

    Update FollowedSeries.tsx

--- src/components/FollowedSeries.tsx
+++ src/components/FollowedSeries.tsx
@@ -3,31 +3,31 @@ import { useSearchParams } from 'react-router-dom';
 import {options} from "../App";
 
 export default function FollowedSeries() {
+    
     const [location, setLocation] = useSearchParams();
+    const [series, setSeries] = useState([]);
 
-  const [series, setSeries] = useState([]);
-
-  const getSeriesBySearch = async (location: URLSearchParams) => {
-    return await fetch(`https://api.themoviedb.org/3/search/tv?include_adult=true&language=en-US&query=${location.get("search")}`, options)
-        .then(response => response.json())
-        .then(data => data.results)
-        .catch(error => console.log(error))
-}
-
-  useEffect(() => {
-    if (location.has("search")) {
-      getSeriesBySearch(location).then(series => setSeries(series))
-      return
+    const getSeriesBySearch = async (location: URLSearchParams) => {
+        return await fetch(`https://api.themoviedb.org/3/search/tv?include_adult=true&language=en-US&query=${location.get("search")}`, options)
+            .then(response => response.json())
+            .then(data => data.results)
+            .catch(error => console.log(error))
     }
-    getSeriesBySearch(location).then(series => setSeries(series))
-  }, [location]);
 
-  return (
-    <div>
-      <h1>Followed Series</h1>
-      <ul>
-        {series}
-      </ul>
-    </div>
-  );
+    useEffect(() => {
+        if (location.has("search")) {
+        getSeriesBySearch(location).then(series => setSeries(series))
+        return
+        }
+        getSeriesBySearch(location).then(series => setSeries(series))
+    }, [location]);
+
+    return (
+        <div>
+        <h1>Followed Series</h1>
+        <ul>
+            {series}
+        </ul>
+        </div>
+    );
 }
