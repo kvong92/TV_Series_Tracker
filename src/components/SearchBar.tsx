b559e8b0ddfabb2e88459b80af9f523580eee14d
Léandre <leandre.colard@gmail.com>
2023-10-20 09:25:57 +0200

    rounded

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,8 +22,8 @@ export const SearchBar: React.FC = () => {
   }
 
   return (
-    <form className="py-9 w-full h-full" onSubmit={onSearch}>
-        <button type='submit' className="p-4 focus:outline-none text-black absolute">
+    <form className="py-9 w-full h-full rounded-full" onSubmit={onSearch}>
+        <button type='submit' className="rounded-full p-4 focus:outline-none text-black absolute">
           <BiSearch />
         </button>
         <input
@@ -31,7 +31,7 @@ export const SearchBar: React.FC = () => {
           onChange={(e) => setQuery(e.target.value)}
           type="search"
           placeholder="Type to search..."
-          className="pl-10 w-full h-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:border-blue-400"
+          className="rounded-full pl-10 w-full h-full px-3 py-2 bg-inherit border border-gray-300 shadow-sm focus:outline-none focus:border-blue-400"
         />
     </form>
   );
